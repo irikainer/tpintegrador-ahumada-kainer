994ac43ecd037db9c9e89e1f838df645e67a149b
Irina Kainer <kainer.irina@gmail.com>
2021-10-14 21:03:33 -0300

    Fix en login

--- login.php
+++ login.php
@@ -2,12 +2,12 @@
 require_once 'clases/usuario/ControladorSesion.php';
 
 if (!isset($_POST['usuario']) || !isset($_POST['clave'])) {
-    $redirigir = 'index.html?mensaje=Error: Falta un campo obligatorio';
+    $redirigir = 'index.php?mensaje=Error: Falta un campo obligatorio';
 } else {
     $cs = new ControladorSesion();
     $login = $cs->login($_POST['usuario'], $_POST['clave']);
     if ($login[0] === true) {
-        $redirigir = 'home.html';
+        $redirigir = 'home.php';
     } else {
         $redirigir = 'index.php?mensaje=' . $login[1];
     }
