d50c18daabe8be7a48ed40bab880cd4f1daa9c2b
nato <kevinnatanaelahumada@hotmail.com>
2021-10-15 18:03:51 -0300

    herencia de repositorio

---
+++ clases/repositorio.php
@@ -0,0 +1,28 @@
+<?php
+require_once '.env.php';
+
+class Repositorio
+{
+    private static $conexion = null;
+
+    public function __construct()
+    {
+        if (is_null(self::$conexion)) {
+            $credenciales = credenciales();
+            self::$conexion = new mysqli(
+                $credenciales['servidor'],
+                $credenciales['usuario'],
+                $credenciales['clave'],
+                $credenciales['base_de_datos']
+            );
+            if (self::$conexion->connect_error) {
+                $error = 'Error de conexión: ' . self::$conexion->connect_error;
+                self::$conexion = null;
+                die($error);
+            }
+            echo "Conexión exitosa";
+            self::$conexion->set_charset('utf8');
+            return self::$conexion;
+        }
+    }
+}
--- clases/usuario/RepositorioUsuario.php
+++ clases/usuario/RepositorioUsuario.php
@@ -1,28 +1,14 @@
 <?php
-require_once '.env.php';
+require_once 'clase/repostorio.php';
 require_once 'Usuario.php';
 
-class RepositorioUsuario
+class RepositorioUsuario extends Repositorio
 {
     private static $conexion = null;
 
     public function __construct()
     {
-        if (is_null(self::$conexion)) {
-            $credenciales = credenciales();
-            self::$conexion = new mysqli(
-                $credenciales['servidor'],
-                $credenciales['usuario'],
-                $credenciales['clave'],
-                $credenciales['base_de_datos']
-            );
-            if (self::$conexion->connect_error) {
-                $error = 'Error de conexión: ' . self::$conexion->connect_error;
-                self::$conexion = null;
-                die($error);
-            }
-            self::$conexion->set_charset('utf8');
-        }
+        self::$conexion = parent::__construct();
     }
 
     public function login($nombre_usuario, $clave)
